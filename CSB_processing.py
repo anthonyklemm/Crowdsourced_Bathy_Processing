1679639a1a2b669825a76ea4d1c8506214bc964f
Anthony Klemm <anthony.klemm@gmail.com>
2023-02-07 10:49:18 -0500

    Add files via upload
    Created a GUI and added code to delete interim rasters created during the BAGextract(): function.

--- CSB_processing.py
+++ CSB_processing.py
@@ -1,42 +1,65 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 28 13:57:44 2020
-Updated 2/2/2023
+Updated 2/6/2023
 
 This script automates the tide correction of crowdsourced bathymetry (CSB) files (in CSV format)
 downloaded from the International Hydrographic Organization's (IHO) Data Centre for Digital Bathymetry 
 Crowdsourced Bathymetry Database. 
 
-The raw CSB data is tide corrected, and then compared against known bathymetry (in BAG format) in a common high-traffic area.
-The mean difference and standard deviation is tabulated for each contirbutor vessel, and any mean difference
+The raw CSB data is tide corrected using a discrete zoned tides model, and then compared against known 
+bathymetry (in BAG format) in a common high-traffic area.
+The mean difference and standard deviation is tabulated for each contributor vessel, and any mean difference
 that has a standard deviation less than 2m is used as a vertical static transducer offset value for that vessel.
 Those vertical offsets are applied to the tide-corrected data to create a CSB bathymetry solution.
 
 The output is a shapefile and a basic geotiff without sophisticated interpolation (recommend changing to IDW). 
-The next development step is to create a GUI for this script. 
+The next development step is to compile the script as a standalone executable. 
 
 @author: Anthony Klemm
 """
 
-
+import PySimpleGUI as sg
 import geopandas as gpd
 import pandas as pd
 import requests
+import sys
 import os
 from osgeo import gdal
 from rasterstats import point_query
-#from functools import partial
 from geocube.api.core import make_geocube
-#from geocube.rasterize import rasterize_points_griddata
 
+sg.theme('DarkTeal2')
+if len(sys.argv) == 1:
+    event, values = sg.Window('CSB Processing Input Files',
+                    [[sg.Text('Title of CSB Data', size=(35,1)), sg.InputText()],
+                    [sg.Text('EPSG Code for output geotiff CRS', size=(35,1)), sg.InputText()],
+                    [sg.Text('Raw CSB data in *.csv format')],
+                    [sg.In(), sg.FileBrowse()],
+                    [sg.Text('Input BAG file for comparison bathymetry')],
+                    [sg.In(), sg.FileBrowse()],
+                    [sg.Text('Tide Zone file in *.shp format')],
+                    [sg.In(), sg.FileBrowse()],
+                    [sg.Text('Specify output folder')],
+                    [sg.In(), sg.FolderBrowse()],
+                    [sg.Open(), sg.Cancel()]]).read(close=True)
+    
+    title = values[0]
+    output_crs = "epsg:"+values[1]
+    csb = values[2]
+    BAG_filepath = values[3]
+    fp_zones = values[4]
+    output_dir = values[5]
+else:
+    csb = sys.argv[1]
+
+if not csb:
+    sg.popup("Cancel", "No filename for csb raw file supplied")
+    raise SystemExit("Cancelling: no filename supplied")
+else:
+    sg.popup('The CSB filename you chose was', csb)
 
 pd.set_option('display.max_columns', None)
-title = 'Kivalina_Alaska_1'
-
-fp_zones = r"F:\csb\tide zone polygons\tide_zone_polygons_new_WGS84.shp"
-csb = r"F:\csb\raw\Kivalina_AK.csv"
-BAG_filepath = "F:/csb/comparison data/H12520_MBVB_4m_MLLW_Combined.bag"
-output_crs="epsg:26903" #change the desired output crs for final raster (recommend NAD83 UTM XX N/S)
 resolution = 50 #desired resolution of output raster in the units of your output_crs (meters or decimal degrees, most likely)
 
 def loadCSB():
@@ -166,16 +189,23 @@ def BAGextract():
     
     #should try and not create so many actual files (keep rasters in memory instead)
     dataset = gdal.Open(BAG_filepath, gdal.GA_ReadOnly)
-    gdal.Translate('E:/CSB_13SEPT2021/' + title+ '.tif', dataset) #converts BAG to dual band geotiff
-    dsReprj = gdal.Warp('E:/CSB_13SEPT2021/' + title+'_5m_MLLW.tif', 'E:/CSB_13SEPT2021/' + title + '.tif', xRes = 8, yRes=8) #resamples the input geotiff to chosen raster resolution
-    
-    str1 = "gdal_translate -b 1 -of AAIGrid " + 'E:/CSB_13SEPT2021/' + title +'_5m_MLLW.tif E:/CSB_13SEPT2021/' + title + '_MB_5m_elevation.tif'
+    gdal.Translate(output_dir +'/'+ title+ '.tif', dataset) #converts BAG to dual band geotiff
+    dataset = None
+    dsReprj = gdal.Warp(output_dir + '/' + title + '_5m_MLLW.tif', output_dir + '/' + title + '.tif', xRes = 8, yRes=8) #resamples the input geotiff to chosen raster resolution
+    str1 = "gdal_translate -b 1 -of AAIGrid " + output_dir + '/' + title +'_5m_MLLW.tif '+ output_dir + '/' + title + '_MB_5m_elevation.tif'
+    #str1 = "gdal_translate -b 1 -of AAIGrid " + 'E:/CSB_13SEPT2021/' + title +'_5m_MLLW.tif E:/CSB_13SEPT2021/' + title + '_MB_5m_elevation.tif'
+    dsReprj = None
     os.system(str1) #extracts the elevation band (band1) and saves as new geotiff
-    raster_file = 'E:/CSB_13SEPT2021/' + title + '_MB_5m_elevation.tif'
+    raster_file = output_dir + '/' + title + '_MB_5m_elevation.tif'
     input_raster = gdal.Open(raster_file)
-    output_raster = 'E:/CSB_13SEPT2021/' + title + '_MB_5m_elevation_wgs84.tif'
+    output_raster = output_dir + '/' + title + '_MB_5m_elevation_wgs84.tif'
     warp = gdal.Warp(output_raster, input_raster, dstSRS='EPSG:4326')
+    raster_file = None
+    input_raster = None
     warp = None
+    os.remove(output_dir + '/' + title + '_5m_MLLW.tif')
+    os.remove(output_dir +'/'+ title+ '.tif')
+    os.remove(output_dir + '/' + title + '_MB_5m_elevation.tif')
     print('****bathy file processed, 5m geotiff creation complete****')
     return output_raster
 
@@ -193,21 +223,22 @@ def derive_draft():
     print(out)
     #should append Master Vessel Offset Table instead of creating individual tables per area
     #how to update fields (mean diff and stdev) only where count is greater than existing record???
-    out.to_csv('E:/CSB_13SEPT2021/VESSEL_OFFSETS_csb_corr_'+ title +'.csv', mode = 'a')
+    out.to_csv(output_dir +'/VESSEL_OFFSETS_csb_corr_'+ title +'.csv', mode = 'a')
     return(csb_corr)
 
 def draft_corr():    
     #join vessel static offset table with csb data (1m std threshold for defining vertical transducer offset)
-    #df = pd.read_csv(r"E:\CSB_13SEPT2021\VESSEL_OFFSETS_csb_corr_Toksook_Bay_Alaska_1.csv")
+    #need to add in code to create and look for draft values in a master data-derived transducer offset table
     csb_corr = derive_draft()
-    df = pd.read_csv('E:/CSB_13SEPT2021/VESSEL_OFFSETS_csb_corr_'+ title +'.csv')
+    df = pd.read_csv(output_dir + "/VESSEL_OFFSETS_csb_corr_"+ title +'.csv')
+    #df_master = pd.read_csv('master_vessel_offset_file.csv')
     df['std'] = pd.to_numeric(df['std'], errors='coerce').astype('float')
     df['mean'] = pd.to_numeric(df['mean'], errors='coerce').astype('float')
     csb_corr1 = pd.merge(csb_corr, df[df['std'] < 5.0], on='platform')
     csb_corr1['depth_final'] = ((csb_corr1['depth_new'] - csb_corr1['mean'])*-1) #correct csb bathy with transducer offset
     
     print('*****exporting tide and vessel offset corrected csb to shapefile*****')
-    csb_corr1.to_file('F:/csb/results/csb_OFFSETS_APPLIED_'+ title +'.shp', driver='ESRI Shapefile')
+    csb_corr1.to_file(output_dir + '/csb_OFFSETS_APPLIED_'+ title +'.shp', driver='ESRI Shapefile')
     return(csb_corr1)
     
 def rasterize_CSB():    
@@ -223,7 +254,7 @@ def rasterize_CSB():
         )
         
     geo_grid['depth_final'].plot()
-    geo_grid["depth_final"].rio.to_raster('F:/csb/results/csb_OFFSETS_APPLIED_'+ title + "_1.tif")
+    geo_grid["depth_final"].rio.to_raster(output_dir + '/csb_OFFSETS_APPLIED_'+ title + '.tif')
     
     print('***** DONE! Thanks for all the CSB! *****')
 
