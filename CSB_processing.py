479fb2ce61f2026b1ed5f89a132896e9836411e0
Anthony Klemm <anthony.klemm@gmail.com>
2023-11-16 15:49:50 -0500

    update includes code optimization changes
    This update optimizes the script performance. Processing times take approximately 1/4 the time it took before.

--- CSB_processing.py
+++ CSB_processing.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 28 13:57:44 2020
-Updated 2/6/2023
+Updated 11/13/2023
 
 This script automates the tide correction of crowdsourced bathymetry (CSB) files (in CSV format)
 downloaded from the International Hydrographic Organization's (IHO) Data Centre for Digital Bathymetry 
@@ -22,13 +22,21 @@ The next development step is to compile the script as a standalone executable.
 import PySimpleGUI as sg
 import geopandas as gpd
 import pandas as pd
+import numpy as np
 import requests
 import sys
 import os
-from osgeo import gdal
+from osgeo import gdal, ogr, osr
 from rasterstats import point_query
 from geocube.api.core import make_geocube
 import subprocess
+import time
+import rasterio
+from rasterio.features import shapes
+from shapely.geometry import shape
+from shapely.ops import unary_union
+from scipy.ndimage import binary_dilation
+
 
 
 sg.theme('DarkTeal2')
@@ -60,23 +68,29 @@ if not csb:
     raise SystemExit("Cancelling: no filename supplied")
 else:
     sg.popup('The CSB filename you chose was', csb)
+    start_time = time.time()
+
 
 pd.set_option('display.max_columns', None)
 resolution = 50 #desired resolution of output raster in the units of your output_crs (meters or decimal degrees, most likely)
 
 def loadCSB():
+
     print('*****Reading CSB input csv file***** ')
-    df=gpd.read_file(csb) #read in CSB data in CSV
-    df1=df[df['platform'] != "Anonymous"] #filter out "Anonymous" vessels (can't do a vessel by vessel offset comparison against valid data)
+    df1=gpd.read_file(csb) #read in CSB data in CSV
+    #df1=df[df['platform_name'] != "Anonymous"] #filter out "Anonymous" vessels (can't do a vessel by vessel offset comparison against valid data)
     #Data cleaning = make sure dates are correct
     
     df1 = df1.astype({'depth' : 'float'}) #turn depth field numeric (float64)
     df2 = df1[df1['depth'] > .5] #remove depth values less that 1.2m (filtering out probable noise)
-    df2 = df2[df2['depth'] < 1000] #remove depths values greater than possible depths in the area - this value can change based on area)
+    df2 = df2[df2['depth'] < 1000] #remove depths values greater than possibleNYC depths in the area - this value can change based on area)
     #data cleaning - remove records with erroneous datetimes
-    df2 = df2.astype({'time':'datetime64'})
+    df2 = df2.astype({'time':'datetime64'}, errors='ignore')
+    df2 = df2.dropna(subset=['time'])
     df2 = df2[df2['time'] > '2014']
-    df2 = df2[df2['time'] < '2023']
+    df2 = df2[df2['time'] < '2024']
+    
+    df2 = df2.drop_duplicates(subset=['lon', 'lat', 'depth', 'time', 'unique_id'])
     gdf = gpd.GeoDataFrame(df2, geometry=gpd.points_from_xy(df2.lon, df2.lat)) #create vector point file from lat and lon
     gdf = gdf.set_crs(4326, allow_override=True)
     return gdf
@@ -158,6 +172,7 @@ def tides():
     jtdf = pd.merge_asof(join, tdf, left_on='time', right_on='t')
     jtdf = jtdf.drop(columns=['ControlS_1'])
     jtdf = jtdf.drop(columns=['ControlS_2'])
+    jtdf = jtdf.drop(columns=['DataProv'])
     jtdf = jtdf.dropna()
     jtdf['t_corr'] = jtdf['t'] + pd.to_timedelta(jtdf['ATCorr'], unit='m')
     
@@ -179,22 +194,22 @@ def tides():
     csb_corr = csb_corr[csb_corr['depth'] > 1.5]
     csb_corr = csb_corr[csb_corr['depth'] < 1000]
     csb_corr = csb_corr.rename(columns={'depth':'depth_old'})
-    csb_corr = csb_corr.drop(columns=['index_right','ATCorr','RR','ATCorr2','RR2','DataProv','Shape_Leng','Shape_Area','Shape_Le_1','t','v','t_corr','t_new','v_new'])
+    csb_corr = csb_corr.drop(columns=['index_right','ATCorr','RR','ATCorr2','RR2','Shape_Leng','Shape_Area','Shape_Le_1','t','v','t_corr','t_new','v_new'])
     return csb_corr
 
 def BAGextract():
-    print('*****Starting to import BAG bathy and aggregate to 5m geotiff*****')
-    #how to convert BAG to geotiff and extract just the elevation layer (and resample if wanted)
-    
-    #should try and not create so many actual files (keep rasters in memory instead)
+    print('*****Starting to import BAG bathy and aggregate to 8m geotiff*****')
+    # Converting BAG to geotiff and extracting the elevation layer
     dataset = gdal.Open(BAG_filepath, gdal.GA_ReadOnly)
-    gdal.Translate(output_dir +'/'+ title+ '.tif', dataset) #converts BAG to dual band geotiff
+    gdal.Translate(output_dir + '/' + title + '.tif', dataset)  # converts BAG to dual band geotiff
     dataset = None
-    dsReprj = gdal.Warp(output_dir + '/' + title + '_5m_MLLW.tif', output_dir + '/' + title + '.tif', xRes = 8, yRes=8) #resamples the input geotiff to chosen raster resolution
-    str1 = "gdal_translate -b 1 -of AAIGrid " + output_dir + '/' + title +'_5m_MLLW.tif '+ output_dir + '/' + title + '_MB_5m_elevation.tif'
-    #str1 = "gdal_translate -b 1 -of AAIGrid " + 'E:/CSB_13SEPT2021/' + title +'_5m_MLLW.tif E:/CSB_13SEPT2021/' + title + '_MB_5m_elevation.tif'
+
+    dsReprj = gdal.Warp(output_dir + '/' + title + '_5m_MLLW.tif', output_dir + '/' + title + '.tif', xRes=8, yRes=8)  # resamples to chosen raster resolution
     dsReprj = None
-    os.system(str1) #extracts the elevation band (band1) and saves as new geotiff
+
+    str1 = "gdal_translate -b 1 -of AAIGrid " + output_dir + '/' + title + '_5m_MLLW.tif ' + output_dir + '/' + title + '_MB_5m_elevation.tif'
+    os.system(str1)  # extracts the elevation band and saves as new geotiff
+
     raster_file = output_dir + '/' + title + '_MB_5m_elevation.tif'
     input_raster = gdal.Open(raster_file)
     output_raster = output_dir + '/' + title + '_MB_5m_elevation_wgs84.tif'
@@ -202,28 +217,93 @@ def BAGextract():
     raster_file = None
     input_raster = None
     warp = None
+
     os.remove(output_dir + '/' + title + '_5m_MLLW.tif')
-    os.remove(output_dir +'/'+ title+ '.tif')
+    os.remove(output_dir + '/' + title + '.tif')
     os.remove(output_dir + '/' + title + '_MB_5m_elevation.tif')
-    print('****bathy file processed, 5m geotiff creation complete****')
-    return output_raster
+
+    output_raster = output_dir + '/' + title + '_MB_5m_elevation_wgs84.tif'
+
+    with rasterio.open(output_raster) as src:
+        array = src.read(1)
+        transform = src.transform
+        nodata = src.nodatavals[0]
+
+        # Create a binary array: 1 where there's data, 0 where there's no data
+        binary_array = np.where(array == nodata, 0, 1).astype('int16')
+
+        # Dilate the binary array and convert to a compatible dtype
+        dilated_array = binary_dilation(binary_array, iterations=25)
+        dilated_array = dilated_array.astype('uint8')  # Convert to uint8
+
+    # Generate shapes from the dilated binary array
+    geometry = []
+    for shp, val in shapes(dilated_array, transform=transform):
+        if val == 1:  # Only consider shapes with value 1 (dilated areas)
+            geom = shape(shp).simplify(tolerance=0.001)  # Apply simplification
+            if geom.is_valid:
+                geometry.append(geom)
+
+    # Create a GeoDataFrame
+    geo_df = gpd.GeoDataFrame(geometry=geometry, crs=src.crs)
+    
+
+    # Save the bathymetry polygon shapefile
+    bathy_polygon_shp = output_dir + '/' + title + '_bathy_polygon.shp'
+    geo_df.to_file(bathy_polygon_shp, driver='ESRI Shapefile')
+
+    print('Bathymetry polygon shapefile created.')
+    return output_raster, bathy_polygon_shp
 
 def derive_draft():
-    output_raster = BAGextract()
+    output_raster, raster_boundary_shp = BAGextract()
     csb_corr = tides()
-    print('*****Starting raster depth extraction at CSB point positions. This step can take a long time depending on how many CSB records are in your file*****')
 
-    pts = point_query(csb_corr, output_raster) #this seems to take a long time
-    print('*****raster depth extraction complete. Calculating and aggregating depth difference statistics. Aggregated results will be stored in the pandas dataframe named: out*****')
-    #next, take resulting pts1 list and add as new column to pts gdf. 
-    csb_corr["Raster_Value"] = pts
-    csb_corr['diff'] = csb_corr['depth_new'] - (csb_corr['Raster_Value'] * -1) #create new column of diff between raster value and observed csb depth
-    out=csb_corr.groupby('platform')['diff'].agg(['mean','std','count']).reset_index() #create new dataframe of vessels showing summary stats for diff
+    print('*****Starting derive_draft function*****')
+    
+    
+    # Load the raster boundary as a GeoDataFrame
+    raster_boundary = gpd.read_file(raster_boundary_shp)
+
+    # Ensure all geometries are valid
+    raster_boundary['geometry'] = raster_boundary['geometry'].apply(lambda geom: geom if geom.is_valid else geom.buffer(0))
+    csb_corr['geometry'] = csb_corr['geometry'].apply(lambda geom: geom if geom.is_valid else geom.buffer(0))
+
+    # Remove any null geometries
+    raster_boundary = raster_boundary[raster_boundary['geometry'].notnull()]
+    #csb_corr = csb_corr[csb_corr['geometry'].notnull()]
+
+    print('Performing the spatial join to create a subset')
+    #csb_corr_subset = csb_corr[csb_corr.geometry.within(raster_boundary.geometry.unary_union)]
+
+
+    # Assign a unique row identifier to the original dataframe
+    csb_corr['row_id'] = range(len(csb_corr))
+
+    # Perform spatial join to create a subset
+    csb_corr_subset = csb_corr[csb_corr.geometry.within(raster_boundary.geometry.unary_union)]
+
+    print('Performing the raster depth extraction with the subset')
+    pts = point_query(csb_corr_subset, output_raster)
+
+    # Add the results to the subset
+    csb_corr_subset["Raster_Value"] = pts
+
+    # Merge the results back into the original dataframe
+    # We will use a left merge to ensure all original records in csb_corr are preserved
+    csb_corr = csb_corr.merge(csb_corr_subset[['row_id', 'Raster_Value']], on='row_id', how='left')
+
+    # Calculate the depth difference with the merged data
+    print('*****Raster depth extraction complete. Calculating depth difference statistics.*****')
+    csb_corr['diff'] = csb_corr['depth_new'] - (csb_corr['Raster_Value'] * -1)
+
+    # Group and calculate statistics efficiently
+    out = csb_corr.groupby('unique_id')['diff'].agg(['mean', 'std', 'count']).reset_index()
+
     print(out)
-    #should append Master Vessel Offset Table instead of creating individual tables per area
-    #how to update fields (mean diff and stdev) only where count is greater than existing record???
-    out.to_csv(output_dir +'/VESSEL_OFFSETS_csb_corr_'+ title +'.csv', mode = 'a')
-    return(csb_corr)
+    out.to_csv(output_dir + '/VESSEL_OFFSETS_csb_corr_' + title + '.csv', mode='a')
+
+    return csb_corr
 
 def draft_corr():    
     #join vessel static offset table with csb data (1m std threshold for defining vertical transducer offset)
@@ -233,7 +313,7 @@ def draft_corr():
     #df_master = pd.read_csv('master_vessel_offset_file.csv')
     df['std'] = pd.to_numeric(df['std'], errors='coerce').astype('float')
     df['mean'] = pd.to_numeric(df['mean'], errors='coerce').astype('float')
-    csb_corr1 = pd.merge(csb_corr, df[df['std'] < 5.0], on='platform')
+    csb_corr1 = pd.merge(csb_corr, df[df['std'] < 5.0], on='unique_id')
     csb_corr1['depth_final'] = ((csb_corr1['depth_new'] - csb_corr1['mean'])*-1) #correct csb bathy with transducer offset
     
     print('*****exporting tide and vessel offset corrected csb to shapefile*****')
@@ -256,8 +336,13 @@ def rasterize_CSB():
     geo_grid["depth_final"].rio.to_raster(output_dir + '/csb_OFFSETS_APPLIED_'+ title + '.tif')
     mod_output_dir = output_dir.replace("/", "\\")
     subprocess.Popen(r'explorer "'+ mod_output_dir)
+    end_time = time.time()
+    execution_time = end_time - start_time
     
+
+
     print('***** DONE! Thanks for all the CSB! *****')
+    print(f"Total execution time: {execution_time} seconds")
 
 if __name__ == "__main__":
     rasterize_CSB()
