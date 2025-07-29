4e12ea8fe7807b58bf6d5c240ae248fdd58c652c
Anthony Klemm <anthony.klemm@gmail.com>
2025-07-29 11:09:44 -0400

    Update fes_model.py

--- fes_model.py
+++ fes_model.py
@@ -11,22 +11,22 @@ def get_fes_tide(lons, lats, times, fes_data_path, template_yaml_path):
     including the necessary longitude conversion.
     """
     try:
-        # 1. Set the environment variable that the YAML file is looking for.
+        
         os.environ['DATASET_DIR'] = fes_data_path
         
-        # 2. Load the configuration from the YAML file.
+        
         handlers = pyfes.load_config(template_yaml_path)
 
-        # 3. --- CRUCIAL FIX: Convert longitude from -180/180 to 0/360 ---
+        
         lons_360 = np.mod(lons, 360)
 
-        # 4. Evaluate the tide using the converted longitudes.
+        
         tide, lp, _ = pyfes.evaluate_tide(handlers['tide'],
                                           times,
-                                          lons_360, # Use the converted array
+                                          lons_360,
                                           lats)
         
-        # 5. Calculate the final tide value in meters.
+        
         pure_tide_cm = tide + lp
         pure_tide_m = pure_tide_cm / 100.0
         
